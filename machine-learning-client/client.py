2b225845c99d4d25611cb9533c3982ddb59c8724
KyleC55 <kc5371@nyu.edu>
2026-04-09 16:45:20 -0400

    Installed python package black

--- machine-learning-client/client.py
+++ machine-learning-client/client.py
@@ -1,19 +1,15 @@
-'''
-Script that connects to Database 
-'''
+"""
+Script that connects to Database
+"""
 
 from datetime import datetime
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://admin:secret@mongo:27017/?authSource=admin")
-db = client['mydatabase']
-collection = db['records']
+db = client["mydatabase"]
+collection = db["records"]
 
-result = {
-    'timestamp': datetime.now(),
-    'label': 'test_detection',
-    'confidence': 0.95
-}
+result = {"timestamp": datetime.now(), "label": "test_detection", "confidence": 0.95}
 
 collection.insert_one(result)
 print("Result inserted into MongoDB")
--- machine-learning-client/test_mongo.py
+++ machine-learning-client/test_mongo.py
@@ -1,11 +1,12 @@
 """
-This is a test file to make sure the DB is working and conected 
+This is a test file to make sure the DB is working and conected
 """
+
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://admin:secret@localhost:27017/')
-db = client['mydatabase']
-collection = db['records']
+client = MongoClient("mongodb://admin:secret@localhost:27017/")
+db = client["mydatabase"]
+collection = db["records"]
 
-collection.insert_one({'name': 'Alice', 'age': 30})
+collection.insert_one({"name": "Alice", "age": 30})
 print("Inserted")
