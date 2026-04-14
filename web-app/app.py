2116fefd02493dd152aea4cc9876a8e440f226e3
17faizan <faizanshamsi2004@gmail.com>
2026-04-14 16:18:02 -0400

    db + flask connection

--- web-app/app.py
+++ web-app/app.py
@@ -1,6 +1,6 @@
 import os
 import pymongo
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,10 +15,30 @@ def create_app(test_config=None):
         app.config.update(test_config)
 
     mongo_uri = os.getenv("MONGO_URI")
-    connection = pymongo.MongoClient(mongo_uri)
-    app.db = None
-    ''''change to app.db = connection[actual name] '''
+    db_name = os.getenv("DB_NAME", "emotion_db") 
+    collection_name = os.getenv('MONGO_COLLECTION', 'scans')
 
+    app.db = None
+    app.collection_name = collection_name
+    ''''change to app.db = connection[actual name] '''
+    try:
+        connection = pymongo.MongoClient(mongo_uri)
+        app.db = connection[db_name]
+    except Exception as exc:
+        print(f"Failed to connect to MongoDB: {exc}")
+
+    @app.route("/debug")
+    def debug():
+        if app.db is None:
+            return {"error": "no db"}
+
+        scans = list(app.db["scans"].find())
+
+        for scan in scans:
+            scan["_id"] = str(scan["_id"])
+
+        return jsonify(scans)
+    
     @app.route("/")
     def home():
         """
@@ -27,7 +47,7 @@ def create_app(test_config=None):
         """
         if app.db is not None:
 
-            activities = list(app.db.analysis.find().limit(10))
+            activities = list(app.db.scans.find().limit(10))
         else:
             activities = [
                 {"name": "System", "message": "Running in offline mode (No DB)"},
@@ -43,5 +63,5 @@ app = create_app()
 
 if __name__ == "__main__":
 
-    flask_port = int(os.getenv("FLASK_PORT", "5000"))
+    flask_port = int(os.getenv("FLASK_PORT", "5001"))
     app.run(host="0.0.0.0", port=flask_port)
