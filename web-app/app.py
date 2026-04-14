0642ff43475d2b81a2c844bb6d1ae60320af42d2
sanabriageorge <gas9907@nyu.edu>
2026-04-14 15:01:59 -0400

    Fix Flask import

--- web-app/app.py
+++ web-app/app.py
@@ -2,9 +2,7 @@
 
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
@@ -14,4 +12,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
