b685752e281bfbb1978301c88dc502299c33c137
NarezIn <zn2133@nyu.edu>
2026-04-18 18:18:58 -0400

    update endpoint to deal with audio upload route and corresponding unit tests

--- web-app/app/routes.py
+++ web-app/app/routes.py
@@ -6,7 +6,9 @@ Defines all HTTP API endpoints for the web application:
 The main interface between the frontend and backend services.
 """
 
-from flask import Blueprint, jsonify  # , request
+from flask import Blueprint, jsonify, request
+from requests.exceptions import RequestException
+from app.services import transcribe_audio
 
 main = Blueprint("main", __name__)
 
@@ -14,9 +16,27 @@ main = Blueprint("main", __name__)
 @main.route("/upload", methods=["POST"])
 def upload_audio():
     """
-    Handles audio file upload and returns speech analysis results.
+    Receives audio file from frontend, sends it to ML service,
+    and returns transcription result.
     """
-    return jsonify({"message": "Placeholder!!!"}), 200
+    file = request.files.get("audio")
+
+    if not file:
+        return jsonify({"error": "No audio file provided"}), 400
+
+    try:
+        transcript = transcribe_audio(file)
+    except RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+    return (
+        jsonify(
+            {
+                "transcript": transcript,  # maybe more fields later. talk with frontend and ml-client.
+            }
+        ),
+        200,
+    )
 
 
 @main.route("/dashboard", methods=["GET"])
---
+++ web-app/tests/test_routes.py
@@ -0,0 +1,28 @@
+"""
+This file is for testing app/routes.py.
+"""
+
+import io
+from app import create_app
+
+
+def test_upload_audio_route(monkeypatch):
+    """
+    Test /upload endpoint with mocked transcription service.
+    """
+
+    app = create_app()
+    client = app.test_client()
+
+    # mock transcribe_audio
+    def mock_transcribe(_):
+        return "hello world"
+
+    monkeypatch.setattr("app.routes.transcribe_audio", mock_transcribe)
+
+    data = {"audio": (io.BytesIO(b"fake audio"), "test.wav")}
+
+    response = client.post("/upload", data=data, content_type="multipart/form-data")
+
+    assert response.status_code == 200
+    assert response.json["transcript"] == "hello world"
